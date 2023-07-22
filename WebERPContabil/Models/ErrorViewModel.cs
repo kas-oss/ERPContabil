437d7d12a34e91cbd500d93266637f8c17940d2e
lucas-seo@hotmail.com.br <kasoss@outlook.com.br>
2023-07-22 14:50:06 -0300

    add mvc web app

---
+++ WebERPContabil/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebERPContabil.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
