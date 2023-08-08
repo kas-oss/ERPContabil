39da63d67e889302f0ac7d44d6f36f2cb0ab5469
lucas-seo@hotmail.com.br <kasoss@outlook.com.br>
2023-08-08 18:20:49 -0300

    ajustes no front

---
+++ UI/WEB_ERP_CONTABIL/Controllers/EmpresaController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WEB_ERP_CONTABIL.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        // GET: EmpresaController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: EmpresaController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: EmpresaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmpresaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmpresaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmpresaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmpresaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmpresaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ UI/WEB_ERP_CONTABIL/Models/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace WEB_ERP_CONTABIL.Models
+{
+    public class Cliente
+    {
+        public int ClienteID { get; set; }
+
+        [DisplayName("Nome")]
+        public string? Nome { get; set; }
+
+        [DisplayName("Sobrenome")]
+        public string? Sobrenome { get; set; }
+
+        [DisplayName("Data de Nascimento")]        
+        public DateTime DataNascimento { get; set; }
+    }
+}
